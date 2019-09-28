7f9f7c1140970dee54b787932360892493b5a952
gaelle <gaelle.rauffet@kiwiscan.net>
2019-09-28 12:49:59 +0200

    structure de l'application pour jouer une partie

---
+++ src/main/java/dev/gaelle_rauffet/escape_game/Game.java
@@ -0,0 +1,62 @@
+package dev.gaelle_rauffet.escape_game;
+
+public class Game {
+	int modeDev = 1;
+	int nbTests = 4;
+	int nbX = 4;
+	
+	
+	/**
+	 * Run a game
+	 */
+	public void run() {
+		loadConfig();
+		displayStartdMsg();
+		int gameMode = 1;
+		runMode(gameMode);
+		displayEndMsg();
+	}
+	
+	/**
+	 * Load a configuration file
+	 */
+	private void loadConfig() {
+		// load a properties files
+		// set attributes with properties values
+		
+	}
+
+	/**
+	 * Display message at starting game
+	 */
+	private void displayStartdMsg() {
+		String[] gameModes = {"challenger","défenseur","duel"};
+		System.out.println("Choississez le mode de jeu : ");
+		for(int i = 0; i < gameModes.length; i++) {
+			System.out.println((i+1) + " - " + gameModes[i]);
+		}
+		
+	}
+
+	/**
+	 * Display message at ending game
+	 */
+	private void displayEndMsg() {
+		String[] endOptions = {"Rejouer au même mode","Rejouer avec un mode différent","Quitter l'application"};
+		System.out.println("Fin de partie : ");
+		for(int i = 0; i < endOptions.length; i++) {
+			System.out.println((i+1) + " - " + endOptions[i]);
+		}
+		
+	}
+
+	/**
+	 * Run a game mode
+	 * 
+	 * @param mode
+	 */
+	public void runMode(int mode) {
+		
+	}
+
+}
---
+++ src/main/java/dev/gaelle_rauffet/escape_game/Main.java
@@ -0,0 +1,10 @@
+package dev.gaelle_rauffet.escape_game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.run();
+	}
+
+}
